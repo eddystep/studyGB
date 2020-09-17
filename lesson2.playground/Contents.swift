4dcbc818b93f6687dc53d8d3a93809a5db5e7d25
Edgar Stepanov <eddyst@MacBook-Pro-Edgar.local>
2020-09-17 13:33:58 +0300

    add lesson2.playground

---
+++ lesson2.playground/Contents.swift
@@ -0,0 +1,86 @@
+import UIKit
+
+//Задание №1
+func chetnechet (a: Int) {
+    if a % 2 == 0 {
+        print("Число четное")
+    } else {
+        print("Число нечетное")
+    }
+}
+
+chetnechet(a: 6)
+
+//Задание №2
+func delenienatri (b: Int) {
+    if b % 3 == 0 {
+        print("Число делится на 3 без остатка")
+    } else {
+        print("К сожалению данное число не делится на 3")
+    }
+}
+delenienatri(b: 13)
+
+//Задание №3
+var array = [Int]()
+var c = 0
+for _ in 1...100 {
+    c += 1
+    array.append(c)
+}
+
+print(array)
+
+//Задание №4
+
+var d:Int = array.count
+while d != 0 {
+    d -= 1
+    if array[d] % 2 == 0 || array[d] % 3 != 0 {
+        array.remove(at: d)
+    }
+}
+ print(array)
+
+//Задание №5
+var fib1 = 1
+var fib2 = 1
+var array = [fib1, fib2]
+var n = 0
+
+func fibonaci() {
+    var sumfib = fib1 + fib2
+    fib1 = fib2
+    fib2 = sumfib
+    array.append(fib2)
+}
+
+while n < 50 {
+    fibonaci()
+    n += 1
+}
+
+print(array)
+
+//Задание №6
+
+var array = [Int]()
+var p = 1
+
+func prostoe(_ number: Int) -> Bool {
+return number > 1 && !(2..<number).contains { number % $0 == 0 }
+}
+
+repeat {
+    p += 1
+    if prostoe(p) == true {
+        array.append(p)
+    }
+    
+} while array.count < 100
+
+
+print(array)
+
+
+
